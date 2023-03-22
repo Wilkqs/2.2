944cbc5f49d712299d8e897a840af1871bef4a7e
Wilkqs <127328280+Wilkqs@users.noreply.github.com>
2023-03-22 15:28:27 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    double a = 5;
+    double b =10;
+    double suma = a+b;
+    double roznica = a-b;
+    double mnozenie = a*b;
+    double dzielenie = a/b;
+
+    System.out.println("Suma="+suma);
+    System.out.println("roznica="+roznica);
+    System.out.println("mnozenie="+mnozenie);
+    System.out.println("dzielenie="+dzielenie);
+    
+      
+  }
+}
